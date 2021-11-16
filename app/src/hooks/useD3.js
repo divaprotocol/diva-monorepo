ea8f08a13bf848dbf3cc34272d9d25dedbe366f5
Julian Krispel-Samsel <julian@jkrsp.com>
2021-11-16 11:20:53 +0000

    remove app folder

--- app/src/hooks/useD3.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import * as d3 from 'd3'
-
-export const useD3 = (renderChartFn, dependencies) => {
-  const ref = React.useRef()
-
-  React.useEffect(() => renderChartFn(d3.select(ref.current)), dependencies)
-  return ref
-}
