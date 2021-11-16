162be14d97b55218e437a8e6bbecc8cd8f78c046
Julian Krispel-Samsel <julian@jkrsp.com>
2021-11-16 11:20:53 +0000

    remove app folder

--- app/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tradeReducer from './TradeOption'
-import activeTabReducer from './ActiveTab'
-export default configureStore({
-  reducer: {
-    tradeOption: tradeReducer,
-    activeTab: activeTabReducer,
-  },
-})
