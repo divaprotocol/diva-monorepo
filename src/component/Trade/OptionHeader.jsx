597fe99cc2b1b6ba18fcedbca713dbbc6c59992a
coha <aleksandar.coha@helbiz.com>
2021-11-24 13:01:26 +0100

    Display position token address with href to etherscan and copy

--- src/component/Trade/OptionHeader.jsx
+++ src/component/Trade/OptionHeader.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import '../../Util/Dates'
 import { useSelector } from 'react-redux'
-
+import { IconButton, Link } from '@mui/material'
+import {
+  getEtherscanLink,
+  EtherscanLinkType,
+} from '../../Util/getEtherscanLink'
+import { useWeb3React } from '@web3-react/core'
 const AppHeader = styled.header`
   min-height: 10vh;
   display: flex;
@@ -70,12 +75,41 @@ function renderRefImgs(assetName) {
 
 export default function OptionHeader() {
   //const option = props.optionData
+  const { chainId } = useWeb3React()
   const selectedOption = useSelector((state) => state.tradeOption.option)
   const headerTitle = selectedOption.ReferenceAsset
+  console.log('selectedOption')
+  console.log(selectedOption)
   return (
     <AppHeader>
       {renderRefImgs(headerTitle)}
       <OptionTitle>{headerTitle}</OptionTitle>
+      <Link
+        style={{ color: 'gray' }}
+        underline={'none'}
+        href={getEtherscanLink(
+          chainId,
+          selectedOption.TokenAddress,
+          EtherscanLinkType.ADDRESS
+        )}
+      >
+        {selectedOption.TokenAddress}
+      </Link>
+      <IconButton
+        onClick={() =>
+          navigator.clipboard.writeText(selectedOption.TokenAddress)
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="14"
+          viewBox="0 0 24 24"
+          width="14"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+        </svg>
+      </IconButton>
     </AppHeader>
   )
 }
