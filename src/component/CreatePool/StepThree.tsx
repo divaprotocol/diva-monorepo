860d11f4a77a2509c4da74dcb89ae0579f5dcec8
Julian Krispel-Samsel <julian@jkrsp.com>
2022-01-05 20:15:18 +0000

    wip

--- src/component/CreatePool/StepThree.tsx
+++ src/component/CreatePool/StepThree.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@mui/material'
 import { Box } from '@mui/material'
 import Container from '@mui/material/Container'
-import { BigNumber } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
+
+import ERC20 from '../../contracts/abis/ERC20.json'
 import { useDivaContract } from '../../hooks/useDivaContract'
 
 const getExpiryInSeconds = (offsetInSeconds: number) =>
@@ -16,6 +18,7 @@ export function StepThree({
   previous: () => void
 }) {
   const contract = useDivaContract()
+  // const erc20Contract = await ethers.getContractAt(ERC20_ABI, erc20CollateralTokenAddress)
   // INPUT (createContingentPool arguments)
   const inflection = parseEther('100')
   const cap = parseEther('170')
@@ -23,11 +26,21 @@ export function StepThree({
   const collateralBalanceShort = parseEther('0.1')
   const collateralBalanceLong = parseEther('0.1')
   const expiryDate = getExpiryInSeconds(200) // epoch unix timestamp in seconds
-  const shortTokenSupply = parseEther('111.556')
-  const longTokenSupply = parseEther('222.11245')
+  const shortTokenSupply = parseEther('30')
+  const longTokenSupply = parseEther('30')
   const referenceAsset = 'Tesla/USD'
   const collateralToken = '0xaD6D458402F60fD3Bd25163575031ACDce07538D'
   const dataFeedProvider = '0x7543C7cfA57BA2a465BAe9084F6776AEdF5684a1'
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, 3)
+
+  const erc20 = new ethers.Contract(
+    collateralToken,
+    ERC20,
+    provider.getSigner()
+  )
+
+  console.log(expiryDate)
 
   return (
     <Container maxWidth="xs">
@@ -36,28 +49,50 @@ export function StepThree({
         <Button onClick={() => previous()}>Back</Button>
         <Button
           onClick={() => {
-            console.log('hello', contract)
-            contract
-              ?.createContingentPool([
-                inflection,
-                cap,
-                floor,
-                collateralBalanceShort,
-                collateralBalanceLong,
-                expiryDate,
-                shortTokenSupply,
-                longTokenSupply,
-                referenceAsset,
-                collateralToken,
-                dataFeedProvider,
-              ])
-              .then((val) => {
-                console.log('created continent pool')
-                console.log(val)
-              })
-              .catch((err) => {
-                console.error(err.message)
-              })
+            const run = async () => {
+              console.log('hello', contract)
+              const poolCreator = await provider.getSigner()
+              const creatorAddress = await poolCreator.getAddress()
+
+              // Set allowance for DIVA contract
+              erc20
+                .approve(
+                  '0x849b3B2eb813d6d5C70214326B412c6f67feaC03',
+                  collateralBalanceLong.add(collateralBalanceShort)
+                )
+                .then(() => {
+                  console.log('approved')
+                  return erc20.allowance(
+                    creatorAddress,
+                    '0x849b3B2eb813d6d5C70214326B412c6f67feaC03'
+                  )
+                })
+                .then(() => {
+                  console.log('allowed')
+                  contract
+                    ?.createContingentPool([
+                      inflection,
+                      cap,
+                      floor,
+                      collateralBalanceShort,
+                      collateralBalanceLong,
+                      expiryDate,
+                      shortTokenSupply,
+                      longTokenSupply,
+                      referenceAsset,
+                      collateralToken,
+                      dataFeedProvider,
+                    ])
+                    .then((val) => {
+                      console.log('created continent pool')
+                      console.log(val)
+                    })
+                    .catch((err) => {
+                      console.error(err.message)
+                    })
+                })
+            }
+            run()
           }}
         >
           Submit input
