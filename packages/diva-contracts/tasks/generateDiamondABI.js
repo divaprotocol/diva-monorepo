7ac21211c91b4f64b6ef741b2110391a98256b8e
Julian Krispel-Samsel <julian@jkrsp.com>
2022-03-16 12:47:21 +0000

    inherit abis for subgraphs from diva-contracts

--- packages/diva-contracts/tasks/generateDiamondABI.js
+++ packages/diva-contracts/tasks/generateDiamondABI.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const { task } = require("hardhat/config");
+const { execSync } = require("child_process");
+const ERC20 = require("@openzeppelin/contracts/build/contracts/ERC20.json");
 
 const basePath = "/contracts/facets/";
 const libraryBasePath = "/contracts/libraries/";
@@ -27,5 +29,16 @@ task(
   }
   let finalAbi = JSON.stringify(abi);
   fs.writeFileSync("./abis/diamond.json", finalAbi);
-  console.log("ABI written to abi/diamond.json");
+
+  console.info("Diamond ABI written to abis/diamond.json");
+
+  execSync(
+    "cp ./artifacts/contracts/PositionToken.sol/PositionToken.json ./abis/position-token.json"
+  );
+
+  console.info("PositionToken ABI written to ./abis/position-token.json");
+
+  fs.writeFileSync("./abis/erc20.json", JSON.stringify(ERC20.abi));
+
+  console.info("ERC20 ABI written to ./abis/erc20.json");
 });
