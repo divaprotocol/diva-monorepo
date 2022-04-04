40184c25a119533f3a7ac878fd074b73b8297f28
mashttt <52886885+Sascha5@users.noreply.github.com>
2022-04-04 21:38:13 +0200

    cleanup
    -added data to gitignore.
    -removed nodemon
    -typo

--- packages/diva-discord/commands/claimtokens.js
+++ packages/diva-discord/commands/claimtokens.js
@@ -10,7 +10,7 @@ module.exports = {
         //check is user is already registered
         if (dbRegisteredUsers.get(userId) == null) {
             interaction.reply({
-                content:  `Your discord account is is not yet registered.\n `
+                content:  `Your discord account is not yet registered.\n `
                             +`Register with **/register ADDRESS**`,
                 ephemeral: true,
             })
