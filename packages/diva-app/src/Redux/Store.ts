485478b4e878a2c7373b2ab3cd9af5c0fc7a15a6
Julian Krispel-Samsel <julian@jkrsp.com>
2022-04-22 13:11:36 +0100

    update

--- packages/diva-app/src/Redux/Store.ts
+++ packages/diva-app/src/Redux/Store.ts
@@ -1,13 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tradeReducer from './TradeOption'
+import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit'
 import { appSlice, defaultAppState, initialState } from './appSlice'
 import { debounce } from '../lib/debounce'
-const preloadedState = JSON.parse(localStorage.getItem('diva-app-state-local'))
+import { tradeOptionSlice } from './TradeOption'
 
 const validState = (state) => {
   if (
-    preloadedState != null &&
-    preloadedState.appSlice != null &&
+    state != null &&
+    state.appSlice != null &&
     /**
      * validates that all properties from defaultAppState and
      * initialState are there as a way to validate the schema
@@ -15,9 +14,10 @@ const validState = (state) => {
      */
     !Object.keys(initialState).every((key) => {
       const res =
-        preloadedState.appSlice[key] != null &&
+        state.tradeOption != null &&
+        state.appSlice[key] != null &&
         Object.keys(defaultAppState).every((_key) => {
-          const _res = preloadedState.appSlice[key][_key] != null
+          const _res = state.appSlice[key][_key] != null
           return _res
         })
       return res
@@ -26,18 +26,26 @@ const validState = (state) => {
     console.error('previous app state is invalid, resetting it', preloadedState)
     return undefined
   }
+
   return state
 }
 
-const store = configureStore({
-  preloadedState: validState(
-    JSON.parse(localStorage.getItem('diva-app-state-local'))
-  ),
+const preloadedState = validState(
+  JSON.parse(localStorage.getItem('diva-app-state-local'))
+)
+
+const storeConfig: ConfigureStoreOptions = {
   reducer: {
-    tradeOption: tradeReducer,
+    tradeOption: tradeOptionSlice.reducer,
     appSlice: appSlice.reducer,
   },
-})
+}
+
+if (preloadedState != null) {
+  storeConfig.preloadedState = preloadedState
+}
+
+const store = configureStore(storeConfig)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
