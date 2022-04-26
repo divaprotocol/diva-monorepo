366215d404f23bdb1b78031936a6a48eb46bf52d
macroman321 <aleksandar.coha@gmail.com>
2022-04-26 12:43:25 +0200

    fixed a typo and alligned the buttons

--- packages/diva-app/src/component/ApproveActionButtons.tsx
+++ packages/diva-app/src/component/ApproveActionButtons.tsx
@@ -87,7 +87,7 @@ export const ApproveActionButtons = ({
                   onClick={() => {
                     setApproveLoading(true)
                     const token = new ethers.Contract(
-                      pool!.collateralToken.id,
+                      collateralTokenAddress,
                       ERC20,
                       provider.getSigner()
                     )
@@ -200,6 +200,7 @@ export const ApproveActionButtons = ({
       return (
         <div
           style={{
+            paddingTop: theme.spacing(6),
             height: '100px',
             width: '100%',
             display: 'flex',
