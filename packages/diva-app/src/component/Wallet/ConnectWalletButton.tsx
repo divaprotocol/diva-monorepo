7716d86d57a81b067df656997cfd1a61b4083fca
Tarun Yadav <tarunyadav9761@gmail.com>
2022-05-08 16:26:33 +0530

    fix: wallet connect button text

--- packages/diva-app/src/component/Wallet/ConnectWalletButton.tsx
+++ packages/diva-app/src/component/Wallet/ConnectWalletButton.tsx
@@ -1,30 +1,35 @@
 import { LoadingButton } from '@mui/lab'
 import { useConnectionContext } from '../../hooks/useConnectionContext'
-import { selectUserAddress } from '../../Redux/appSlice'
+import { selectChainId, selectUserAddress } from '../../Redux/appSlice'
 import { useAppSelector } from '../../Redux/hooks'
 import { getShortenedAddress } from '../../Util/getShortenedAddress'
 
 export function ConnectWalletButton() {
-  const context = useConnectionContext()
+  const { isConnected, disconnect, connect } = useConnectionContext()
   const userAddress = useAppSelector(selectUserAddress)
+  const chainId = useAppSelector(selectChainId)
 
   return (
     <LoadingButton
       variant="contained"
       color="primary"
       size="large"
-      loading={context.chainId == null}
+      loading={chainId == null}
       type="submit"
       value="Submit"
       sx={{ marginLeft: '10px' }}
-      onClick={() =>
-        context?.isConnected ? context.disconnect() : context.connect()
-      }
+      onClick={() => {
+        if (isConnected && userAddress) {
+          console.log('Disconnecting...')
+          disconnect()
+        } else {
+          console.log('Connecting...')
+          connect()
+        }
+      }}
     >
-      {context?.isConnected
-        ? userAddress != null
-          ? getShortenedAddress(userAddress)
-          : ''
+      {isConnected && userAddress
+        ? getShortenedAddress(userAddress)
         : 'Connect Wallet'}
     </LoadingButton>
   )
