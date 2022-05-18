42445cf46c568122d852bfe9577d7945d76439f9
Ashis Kumar <ashishsahoo389@gmail.com>
2022-05-18 21:25:49 +0530

    Added the Tabpanel component, removed the commented code and pass the value into useState hook using variable.

--- packages/diva-app/src/component/Trade/Underlying.tsx
+++ packages/diva-app/src/component/Trade/Underlying.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import {
-  Box,
   Container,
   Typography,
   Divider,
@@ -9,8 +8,10 @@ import {
   Stack,
   useTheme,
 } from '@mui/material'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
 import Tab from '@mui/material/Tab'
-import Tabs from '@mui/material/Tabs'
 import CreateOrder from './CreateOrder'
 import { useParams } from 'react-router'
 import { useHistory } from 'react-router-dom'
@@ -51,61 +52,21 @@ const RightDiv = styled.div`
   width: 35%;
 `
 
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: number
-  value: number
-}
-
 enum TabPath {
-  trade,
-  liquidity,
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  )
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  }
+  trade = 'trade',
+  liquidity = 'liquidity',
 }
 
 export default function Underlying() {
   const history = useHistory()
   const params: { poolId: string; tokenType: string } = useParams()
   const isLong = params.tokenType === 'long'
-  const CurrentTab = () => {
-    if (
-      history.location.pathname ===
-      `/${params.poolId}/${isLong ? 'long' : 'short'}`
-    )
-      return 0
-    else if (
-      history.location.pathname ===
-      `/${params.poolId}/${isLong ? 'long' : 'short'}/trade`
-    )
-      return 0
-    else if (
-      history.location.pathname ===
-      `/${params.poolId}/${isLong ? 'long' : 'short'}/liquidity`
-    )
-      return 1
-  }
+
+  const CurrentTab =
+    history.location.pathname ===
+    `/${params.poolId}/${isLong ? 'long' : 'short'}/liquidity`
+      ? 'liquidity'
+      : 'trade'
 
   const [value, setValue] = React.useState(CurrentTab)
   const theme = useTheme()
@@ -169,7 +130,7 @@ export default function Underlying() {
 
   const data = generatePayoffChartData(OptionParams)
   const tokenAddress = isLong ? pool.longToken.id : pool.shortToken.id
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     history.push(
       `/${params.poolId}/${isLong ? 'long' : 'short'}/` + TabPath[newValue]
     )
@@ -178,125 +139,135 @@ export default function Underlying() {
 
   return (
     <Container sx={{ paddingTop: '1em', paddingBottom: '3em' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="standard"
-        centered
-        sx={{ mb: theme.spacing(4) }}
-      >
-        <Tab label="Trade" {...a11yProps(0)} />
-        <Tab label="Liquidity" {...a11yProps(1)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <Stack direction="row" spacing={2}>
-          <LeftDiv>
-            <Stack spacing={2}>
-              <Paper>
-                <OptionHeader
-                  ReferenceAsset={pool.referenceAsset}
-                  TokenAddress={tokenAddress}
-                  isLong={isLong}
-                  poolId={pool.id}
-                  tokenDecimals={pool.collateralToken.decimals}
-                />
-                <OptionDetails pool={pool} isLong={isLong} />
-              </Paper>
-              <Paper>
-                <LeftCompFlexContainer>
-                  <OrdersPanel
+      <TabContext value={value}>
+        <TabList
+          onChange={handleChange}
+          variant="standard"
+          centered
+          sx={{ mb: theme.spacing(4) }}
+        >
+          <Tab value="trade" label="Trade" />
+          <Tab value="liquidity" label="Liquidity" />
+        </TabList>
+        <TabPanel value="trade">
+          <Stack direction="row" spacing={2}>
+            <LeftDiv>
+              <Stack spacing={2}>
+                <Paper>
+                  <OptionHeader
+                    ReferenceAsset={pool.referenceAsset}
+                    TokenAddress={tokenAddress}
+                    isLong={isLong}
+                    poolId={pool.id}
+                    tokenDecimals={pool.collateralToken.decimals}
+                  />
+                  <OptionDetails pool={pool} isLong={isLong} />
+                </Paper>
+                <Paper>
+                  <LeftCompFlexContainer>
+                    <OrdersPanel
+                      option={pool}
+                      tokenAddress={tokenAddress}
+                      exchangeProxy={exchangeProxy}
+                    />
+                  </LeftCompFlexContainer>
+                </Paper>
+              </Stack>
+            </LeftDiv>
+            <RightDiv>
+              <Stack spacing={2}>
+                <Paper>
+                  <CreateOrder
                     option={pool}
                     tokenAddress={tokenAddress}
                     exchangeProxy={exchangeProxy}
+                    chainId={chainId}
                   />
-                </LeftCompFlexContainer>
-              </Paper>
-            </Stack>
-          </LeftDiv>
-          <RightDiv>
-            <Stack spacing={2}>
-              <Paper>
-                <CreateOrder
-                  option={pool}
-                  tokenAddress={tokenAddress}
-                  exchangeProxy={exchangeProxy}
-                  chainId={chainId}
-                />
-              </Paper>
-              <Paper>
-                <TradeChart
-                  data={data}
-                  refAsset={pool.referenceAsset}
-                  payOut={pool.collateralToken.symbol}
-                  w={380}
-                  h={200}
-                  isLong={OptionParams.IsLong}
-                  breakEven={breakEvenOptionPrice}
-                />
-              </Paper>
-              <Typography
-                sx={{
-                  paddingLeft: theme.spacing(3),
-                  mt: theme.spacing(1),
-                }}
-              >
-                Buyers statistics:
-              </Typography>
-              <Divider />
-              <Stack direction="row" justifyContent="space-between">
-                <Typography sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}>
-                  Max yield
+                </Paper>
+                <Paper>
+                  <TradeChart
+                    data={data}
+                    refAsset={pool.referenceAsset}
+                    payOut={pool.collateralToken.symbol}
+                    w={380}
+                    h={200}
+                    isLong={OptionParams.IsLong}
+                    breakEven={breakEvenOptionPrice}
+                  />
+                </Paper>
+                <Typography
+                  sx={{
+                    paddingLeft: theme.spacing(3),
+                    mt: theme.spacing(1),
+                  }}
+                >
+                  Buyers statistics:
                 </Typography>
-                {isBuy ? (
+                <Divider />
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography
+                    sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}
+                  >
+                    Max yield
+                  </Typography>
+                  {isBuy ? (
+                    <Typography
+                      sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}
+                    >
+                      {maxYield.buy}
+                    </Typography>
+                  ) : (
+                    <Typography
+                      sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}
+                    >
+                      {maxYield.sell}
+                    </Typography>
+                  )}
+                </Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography
+                    sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}
+                  >
+                    Break-even
+                  </Typography>
                   <Typography
                     sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}
                   >
-                    {maxYield.buy}
+                    {breakEven}
                   </Typography>
-                ) : (
+                </Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography
+                    sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}
+                  >
+                    Intrinsic value per token
+                  </Typography>
                   <Typography
                     sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}
                   >
-                    {maxYield.sell}
+                    {intValDisplay}
                   </Typography>
-                )}
+                </Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography
+                    sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}
+                  >
+                    Max payout per token
+                  </Typography>
+                  <Typography
+                    sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}
+                  >
+                    {maxPayout}
+                  </Typography>
+                </Stack>
               </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}>
-                  Break-even
-                </Typography>
-                <Typography sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}>
-                  {breakEven}
-                </Typography>
-              </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}>
-                  Intrinsic value per token
-                </Typography>
-                <Typography sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}>
-                  {intValDisplay}
-                </Typography>
-              </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography sx={{ ml: theme.spacing(3), mt: theme.spacing(1) }}>
-                  Max payout per token
-                </Typography>
-                <Typography sx={{ mr: theme.spacing(3), mt: theme.spacing(1) }}>
-                  {maxPayout}
-                </Typography>
-              </Stack>
-            </Stack>
-          </RightDiv>
-        </Stack>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Liquidity pool={pool} />
-      </TabPanel>
-      {/*  {value ? (
-        <Liquidity pool={pool} />
-      ) : (
-        
-      )} */}
+            </RightDiv>
+          </Stack>
+        </TabPanel>
+        <TabPanel value="liquidity">
+          <Liquidity pool={pool} />
+        </TabPanel>
+      </TabContext>
     </Container>
   )
 }
