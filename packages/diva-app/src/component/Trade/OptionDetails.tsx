494258578c6ce34fbdcb9e4232b1b54ff3eb2039
macroman321 <aleksandar.coha@gmail.com>
2022-06-03 15:01:02 +0200

    added payoff types in the trade page

--- packages/diva-app/src/component/Trade/OptionDetails.tsx
+++ packages/diva-app/src/component/Trade/OptionDetails.tsx
@@ -162,15 +162,46 @@ export default function OptionDetails({
           <FlexBoxData>{isLong ? 'Up' : 'Down'}</FlexBoxData>
         </FlexBox>
         {binary ? (
-          <FlexBox>
-            <FlexBoxHeader>Payoff type</FlexBoxHeader>
-            <FlexBoxData>Binary</FlexBoxData>
-          </FlexBox>
+          <>
+            <FlexBox>
+              <FlexBoxHeader>Payoff type</FlexBoxHeader>
+              <FlexBoxData>Binary</FlexBoxData>
+            </FlexBox>
+            <FlexBox>
+              <Tooltip
+                placement="top-end"
+                title="Value of the reference asset at which the long token pays out Gradient and the short token 1 - Gradient (see advanced settings)."
+              >
+                <FlexBoxHeader>Inflection</FlexBoxHeader>
+              </Tooltip>
+              <FlexBoxData>{formatEther(pool.inflection)}</FlexBoxData>
+            </FlexBox>
+          </>
         ) : linear ? (
-          <FlexBox>
-            <FlexBoxHeader>Payoff type</FlexBoxHeader>
-            <FlexBoxData>Linear</FlexBoxData>
-          </FlexBox>
+          <>
+            <FlexBox>
+              <FlexBoxHeader>Payoff type</FlexBoxHeader>
+              <FlexBoxData>Linear</FlexBoxData>
+            </FlexBox>
+            <FlexBox>
+              <Tooltip
+                placement="top-end"
+                title="Value of the reference asset at or below which the long token pays out 0 and the short token 1 (max payout)."
+              >
+                <FlexBoxHeader>Floor</FlexBoxHeader>
+              </Tooltip>
+              <FlexBoxData>{formatEther(pool.floor)}</FlexBoxData>
+            </FlexBox>
+            <FlexBox>
+              <Tooltip
+                placement="top-end"
+                title="Value of the reference asset at or above which the long token pays out 1 (max payout) and the short token 0."
+              >
+                <FlexBoxHeader>Cap</FlexBoxHeader>
+              </Tooltip>
+              <FlexBoxData>{formatEther(pool.cap)}</FlexBoxData>
+            </FlexBox>
+          </>
         ) : (
           <>
             <FlexBox>
