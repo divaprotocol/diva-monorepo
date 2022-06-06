c98c32f68058fc490cdc6105b79abc0083beb7fb
Sambit03 <sambit.rath.378@gmail.com>
2022-06-06 11:51:48 +0530

    Update OrderBook.tsx

--- packages/diva-app/src/component/Trade/OrderBook.tsx
+++ packages/diva-app/src/component/Trade/OrderBook.tsx
@@ -291,7 +291,7 @@ export default function OrderBook(props: {
                     <TableCell align="center">
                       <Box>
                         <Typography variant="subtitle1" color="#66ffa6">
-                          {row.bid != '' ? Number(row.bid)?.toFixed(2) : '-'}
+                          {row.bid != '' ? Number(row.bid)?.toFixed(4) : '-'}
                         </Typography>
                         <Typography variant="caption" color="#8e8e8e" noWrap>
                           {row.buyExpiry}
@@ -301,9 +301,7 @@ export default function OrderBook(props: {
                     <TableCell align="center">
                       <Box>
                         <Typography variant="subtitle1" color="#ff5c8d">
-                          {row.ask != ''
-                            ? row.ask?.toFixed(2).toString(4)
-                            : '-'}
+                          {row.ask != '' ? row.ask?.toString(4) : '-'}
                         </Typography>
                         <Typography variant="caption" color="#8e8e8e" noWrap>
                           {row.sellExpiry}
