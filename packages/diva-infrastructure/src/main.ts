62dd15144e06076da88b7eca1f9b9e8c65c9c6e9
Julian Krispel-Samsel <julian@jkrsp.com>
2022-06-11 21:33:22 -0400

    add cloudfront

--- packages/diva-infrastructure/src/main.ts
+++ packages/diva-infrastructure/src/main.ts
@@ -4,6 +4,7 @@ import { AwsProvider } from "@cdktf/provider-aws";
 import { id } from "./lib/id";
 import { orderBookApi } from "./orderBookApi";
 import { S3Bucket } from "@cdktf/provider-aws/lib/s3";
+import { CloudfrontDistribution } from "@cdktf/provider-aws/lib/cloudfront";
 
 class DivaStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -16,18 +17,57 @@ class DivaStack extends TerraformStack {
       dynamodbTable: "TerraformLock",
     });
 
-
     new AwsProvider(this, id("provider"), {
       region: "eu-west-2",
     });
 
-    orderBookApi(this)
+    orderBookApi(this);
 
-    new S3Bucket(this, id("app.diva.finance"), {
+    const bucket = new S3Bucket(this, id("app.diva.finance"), {
       bucket: "app.diva.finance",
       acl: "public-read",
-      website: {
-        indexDocument: "index.html",
+    });
+
+    const originId = id("AppCloudFrontDistro")
+
+    new CloudfrontDistribution(this, id("AppCloudFrontDistro"), {
+      enabled: true,
+      defaultCacheBehavior: {
+        allowedMethods: [
+          "DELETE",
+          "GET",
+          "HEAD",
+          "OPTIONS",
+          "PATCH",
+          "POST",
+          "PUT",
+        ],
+        cachedMethods: ["GET", "HEAD"],
+        forwardedValues: {
+          queryString: false,
+          cookies: {
+            forward: "none",
+          },
+        },
+        viewerProtocolPolicy: "allow-all",
+        minTtl: 0,
+        defaultTtl: 3600,
+        maxTtl: 86400,
+        targetOriginId: originId,
+      },
+      origin: [
+        {
+          domainName: bucket.bucketRegionalDomainName,
+          originId,
+        },
+      ],
+      restrictions: {
+        geoRestriction: {
+          restrictionType: "blacklist",
+        },
+      },
+      viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
       },
     });
   }
