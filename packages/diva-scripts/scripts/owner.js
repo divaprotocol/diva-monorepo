807595378cdc2337ac11662daf995f3d99a1b163
Walodja1987 <wladimir.weinbender@gmail.com>
2022-06-26 21:47:16 +0200

    goerli network in hardhat.config and owner.js script added

---
+++ packages/diva-scripts/scripts/owner.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat");
+const DIVA_ABI = require("@diva/contracts/abis/diamond.json");
+const { addresses } = require("../constants/constants");
+
+async function main() {
+  
+  // INPUTS: network name
+  const network = "goerli" // has to be one of the networks included in constants.js
+
+  // Connect to DIVA contract
+  let diva = await ethers.getContractAt(
+    DIVA_ABI,
+    addresses[network].divaAddress
+  );
+  console.log("DIVA address: ", diva.address);
+
+  // Get current owner
+  console.log("Current owner: " + (await diva.owner()));
+  
+  // TODO Set fallback provider & treasury address
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
