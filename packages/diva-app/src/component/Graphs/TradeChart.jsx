6229960bb23903af85cfda7239d1a845b50e85de
Sambit03 <sambit.rath.378@gmail.com>
2022-07-08 12:33:53 +0530

    Update TradeChart.jsx

--- packages/diva-app/src/component/Graphs/TradeChart.jsx
+++ packages/diva-app/src/component/Graphs/TradeChart.jsx
@@ -90,13 +90,13 @@ export default function DIVATradeChart(props) {
       .attr('cx', 150)
       .attr('cy', h)
       .attr('r', 6)
-      .style('fill', '#1B394F')
+      .style('fill', '#83BD67')
     svg
       .append('circle')
       .attr('cx', 300)
       .attr('cy', h)
       .attr('r', 6)
-      .style('fill', '#4C0D46')
+      .style('fill', '#3393E0')
     svg
       .append('text')
       .attr('x', 20)
@@ -195,7 +195,7 @@ export default function DIVATradeChart(props) {
       .style('stroke', '#B8B8B8')
       .style('stroke-width', '0.75px')
     //for Y axis
-    // for cuurent price point
+    // for current price point
     svg
       .append('g')
       .selectAll('dot')
