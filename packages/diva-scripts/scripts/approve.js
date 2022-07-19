2ce37e262b3d2b41ec253804d022b6453b918988
Walodja1987 <wladimir.weinbender@gmail.com>
2022-07-19 13:21:00 +0200

    approve function added back in approve.js and some of the changes made for debugging reversed

--- packages/diva-scripts/scripts/approve.js
+++ packages/diva-scripts/scripts/approve.js
@@ -18,9 +18,9 @@
 
     // Get signers
     const [acc1, acc2, acc3] = await ethers.getSigners();
-    const user = '0x8836a16db8db1cba0890f0ee97a9926cb47b4101';
+    const user = acc3;
 
-    console.log("Approved by: " + user)
+    console.log("Approved by: " + user.address)
     console.log("Approved for: " + exchangeProxyAddress)
 
     // Connect to token to approve
@@ -31,23 +31,23 @@
     allowance = parseUnits(allowance.toString(), decimals)
     
     // Allowance before
-    const allowanceBefore = await erc20.allowance(user, exchangeProxyAddress)
+    const allowanceBefore = await erc20.allowance(user.address, exchangeProxyAddress)
     console.log("Current approved amount (integer): " + allowanceBefore.toString())
     console.log("Current approved amount (decimals): " + formatUnits(allowanceBefore, decimals))
 
-    // Balance
-    const balance = await erc20.balanceOf(user)
+    // Balance check (can be useful information if balance < allowance)
+    const balance = await erc20.balanceOf(user.address)
     console.log("balance of (integer): ", balance.toString())
     console.log("balance of (decimals): ", formatUnits(balance, decimals))
 
     // Set allowance for exchangeProxyAddress
-    // const tx = await erc20.connect(user).approve(exchangeProxyAddress, allowance)
-    // await tx.wait()
+    const tx = await erc20.connect(user).approve(exchangeProxyAddress, allowance)
+    await tx.wait()
 
-    // // Print
-    // const allowanceAfter = await erc20.allowance(user.address, exchangeProxyAddress)
-    // console.log("New approved amount (integer): " + allowanceAfter.toString())
-    // console.log("New approved amount (decimals): " + formatUnits(allowanceAfter, decimals))
+    // Print
+    const allowanceAfter = await erc20.allowance(user.address, exchangeProxyAddress)
+    console.log("New approved amount (integer): " + allowanceAfter.toString())
+    console.log("New approved amount (decimals): " + formatUnits(allowanceAfter, decimals))
  
  }
  
