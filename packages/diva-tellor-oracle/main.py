131745bc52ada87cfd6fa0f66c4743b5f43c31a4
manvsc <79197645+manvsc@users.noreply.github.com>
2022-07-27 22:17:34 +0200

    added comments

--- packages/diva-tellor-oracle/main.py
+++ packages/diva-tellor-oracle/main.py
@@ -9,15 +9,16 @@ import time
 network = "ropsten"
 w3 = Web3(Web3.HTTPProvider(config.PROVIDER_URL[network]))
 
-poolId = 48696
+poolId = 48696 # TODO: Pull all open expired pools from e.g. theGraph
 
-finalReferenceValue = 93
-collateralToUSDRate = 1
+finalReferenceValue = 93 # TODO: Pull actual value (from e.g. Kraken)
+collateralToUSDRate = 1 # TODO: Pull actual rate
 
 tellor_contract = w3.eth.contract(address=tellor.TellorPlayground_contract_address[network], abi=tellor.TellorPlayground_abi)
 submitTellorValue(poolId, finalReferenceValue, collateralToUSDRate, network, w3, tellor_contract)
 
-time.sleep(15)
+# TODO: Instead of 15 sec, pull the value from the DIVATellorOracle contract (see slack channel)
+time.sleep(15) # After submitting the value there is a delay before you can trigger the function. For testing delay is 10 sec
 
 DIVAOracleTellor_contract = w3.eth.contract(address=tellor.DIVAOracleTellor_contract_address[network], abi=tellor.DIVAOracleTellor_abi)
 setFinRefVal(poolId, network, w3, DIVAOracleTellor_contract)
