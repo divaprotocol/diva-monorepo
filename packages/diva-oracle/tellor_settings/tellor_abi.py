7c54def1e160a5a5289b0aabd00d124c1cd10756
CatLava <evan.schoening@gmail.com>
2022-08-16 08:15:15 -0700

    fixed ABI listing

--- packages/diva-oracle/tellor_settings/tellor_abi.py
+++ packages/diva-oracle/tellor_settings/tellor_abi.py
@@ -1,19 +1,3 @@
-TellorPlayground_contract_address = {
-    "ropsten": '0x7B8AC044ebce66aCdF14197E8De38C1Cc802dB4A',
-    "rinkeby": '0x7B8AC044ebce66aCdF14197E8De38C1Cc802dB4A',
-}
-
-DIVAOracleTellor_contract_address = {
-    "ropsten": '0x638c4aB660A9af1E6D79491462A0904b3dA78bB2', # 10 sec delay (only for testing)
-    "rinkeby": '0x7f4262f9511702a4e69F1AA41adDD653Fbc50cf9',
-    #"ropsten": '0x2f4218C9262216B7B73A76334e5A98F3eF71A61c',
-}
-
-divaDiamond = {
-    "ropsten": '0xebBAA31B1Ebd727A1a42e71dC15E304aD8905211',
-    "rinkeby": '0x3481C73363b52a26068b1C7006CEF98670FEE514',
-}
-
 TellorPlayground_abi = '''[
 {
       "inputs": [
