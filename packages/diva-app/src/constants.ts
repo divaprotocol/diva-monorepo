8e03cefdf9345bd1b2f12d7e99846767819a10e6
Walodja1987 <wladimir.weinbender@gmail.com>
2022-08-20 15:32:18 +0200

    dUSD token address and current supported network updated

--- packages/diva-app/src/constants.ts
+++ packages/diva-app/src/constants.ts
@@ -144,28 +144,6 @@ export const config: { [key: number]: SingleConfig } = {
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     isSupported: false,
   },
-  // 4: {
-  //   name: 'Rinkeby',
-  //   divaAddress: '0x3481C73363b52a26068b1C7006CEF98670FEE514',
-  //   whitelistAddress: '0x5a4385BAf615A35f79787A5cEDFb7ac44Fb26D7e',
-  //   divaSubgraph:
-  //     'https://api.thegraph.com/subgraphs/name/divaprotocol/diva-rinkeby',
-  //   whitelistSubgraph:
-  //     'https://api.thegraph.com/subgraphs/name/divaprotocol/diva-whitelist-rinkeby',
-  //   allOrders: 'https://rinkeby.api.0x.org/orderbook/v1/orders/',
-  //   order: 'https://rinkeby.api.0x.org/orderbook/v1/order/',
-  // },
-  // 42: {
-  //   name: 'Kovan',
-  //   divaAddress: '0x69E0577cAd908D9098F36dfbC4Ec36ad09920F4b',
-  //   whitelistAddress: '0x5a4385BAf615A35f79787A5cEDFb7ac44Fb26D7e',
-  //   divaSubgraph:
-  //     'https://api.thegraph.com/subgraphs/name/divaprotocol/diva-kovan',
-  //   whitelistSubgraph:
-  //     'https://api.thegraph.com/subgraphs/name/divaprotocol/diva-whitelist-kovan',
-  //   allOrders: 'https://kovan.api.0x.org/orderbook/v1/orders/',
-  //   order: 'https://kovan.api.0x.org/orderbook/v1/order/',
-  // },
 }
 
 // array of all chains id
@@ -174,7 +152,7 @@ export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(
 ).filter((id) => typeof id === 'number') as SupportedChainId[]
 
 // current supported chain
-export const CURRENT_SUPPORTED_CHAIN_ID = [SupportedChainId.ROPSTEN]
+export const CURRENT_SUPPORTED_CHAIN_ID = [SupportedChainId.GOERLI]
 
 export const divaGovernanceAddress =
   '0xBb0F479895915F80f6fEb5BABcb0Ad39a0D7eF4E' // creator of pools on Main Markets page and trading fee recipient
