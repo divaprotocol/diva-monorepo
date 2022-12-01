4ebb2c795def09c98d33f70b7d42dc08849808d9
manvsc <manvir.schneider@gmx.de>
2022-12-01 16:02:07 +0100

    update dispute

--- packages/diva-oracle/tellor_settings/tellor_retrieveData.py
+++ packages/diva-oracle/tellor_settings/tellor_retrieveData.py
@@ -21,8 +21,12 @@ def retrieveData(pool_id, network, my_contract):
             timestmp = my_contract.functions.getTimestampbyQueryIdandIndex('0x' + queryId.hexdigest(), idx).call()
             data = my_contract.functions.retrieveData('0x' + queryId.hexdigest(), timestmp).call()
             disp = my_contract.functions.isDisputed('0x' + queryId.hexdigest(), timestmp).call()  # Later we have to use the function isInDispute()
-            val = eth_abi.decode_abi(['uint256', 'uint256'],data)
-            values.append([float(web3.Web3.fromWei(val[0], 'ether')), float(web3.Web3.fromWei(val[1], 'ether')), timestmp, disp])
+            if data != b'':
+                val = eth_abi.decode_abi(['uint256', 'uint256'],data)
+                values.append([float(web3.Web3.fromWei(val[0], 'ether')), float(web3.Web3.fromWei(val[1], 'ether')), timestmp, disp])
+            else:
+                val = ("n/a", "n/a")
+                values.append([val[0], val[1], timestmp, disp])
         return values
 
     except Exception as err:
