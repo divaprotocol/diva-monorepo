bf6e17dce076f3a5773a2bf40d260c2ad7433012
divaprotocol <52886885+Sascha5@users.noreply.github.com>
2023-11-08 21:34:21 +0100

    Bot adjusted to mumbai

--- packages/diva-discord/commands/claim-test-assets.js
+++ packages/diva-discord/commands/claim-test-assets.js
@@ -22,22 +22,22 @@ module.exports = {
             console.log(`Function claimtokens called by ${userName}`)
 
             //check is user is already registered
-            if (dbRegisteredUsers.get(userId) == null) {
-                replyText = `Your discord account is not yet registered.\n `
-                +`Register with **/register ADDRESS**`
-            } else {
-                //next possible claim is 24h after last claim
-                const timeUntilNewClaim = new Date(dbRegisteredUsers.get(userId, "timestampLastClaim")).getTime() + 24 * 60 * 60 * 1000 - new Date()    
-                if ( timeUntilNewClaim > 0 ){
-                    replyText = `You can only claim tokens once per 24h.\n `
-                    +`You need to wait ${new Date(timeUntilNewClaim).toISOString().slice(11,19)} before the next claim`
-                } else {
+            // if (dbRegisteredUsers.get(userId) == null) {
+            //     replyText = `Your discord account is not yet registered.\n `
+            //     +`Register with **/register ADDRESS**`
+            // } else {
+            //     //next possible claim is 24h after last claim
+            //     const timeUntilNewClaim = new Date(dbRegisteredUsers.get(userId, "timestampLastClaim")).getTime() + 24 * 60 * 60 * 1000 - new Date()    
+            //     if ( timeUntilNewClaim > 0 ){
+            //         replyText = `You can only claim tokens once per 24h.\n `
+            //         +`You need to wait ${new Date(timeUntilNewClaim).toISOString().slice(11,19)} before the next claim`
+            //     } else {
                     addToSendQueue = true
                     if (interaction instanceof Discord.Interaction) {
                         await interaction.deferReply({ephemeral: true})
                     }
-                }
-            }
+            //     }
+            // }
 
             console.log(replyText);
             if (replyText != ""){
--- packages/diva-discord/readData.js
+++ packages/diva-discord/readData.js
@@ -10,17 +10,36 @@ const dbRegisteredUsers = new Enmap(
 )
 
 var regUsers = [];
+var users = []
+var regAddress = []
 var counter = 0
+var address = {};
+var duplicatedAddress = {};
 
 dbRegisteredUsers.forEach((value, map) => {
     // stringify JSON Object
     regUsers.push([map, value]);
+    users.push(map)
+    if (value["address"] in duplicatedAddress) {
+        duplicatedAddress[value["address"]] ++
+    }
+    else if (value["address"] in address){
+        duplicatedAddress[value["address"]] = 2
+    } 
+    else {
+        address[value["address"]] = 1
+    }
     counter= counter+1
 });
+console.log(duplicatedAddress)   
 
 
 fs.writeFile("output.json", JSON.stringify(regUsers), 'utf8', function (err) {
     console.log("Number of users: ");
     console.log(counter)    
-    console.log("JSON file has been saved.");
+    console.log("output.json has been saved.");
+    });
+
+fs.writeFile("users.json", JSON.stringify(users), 'utf8', function (err) {
+    console.log("users.json has been saved.");
     });
